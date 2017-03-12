27646da823baa9088fbe796780853f0d8f970a23
Rebecca Mutek <mutek.b@gmail.com>
2017-03-12 11:53:09 -0500

    updated mini-capstone

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,53 @@
 class ProductsController < ApplicationController
   
-  def all_products_method
-    @products = Product.all 
-    render "all_products.html.erb"
+  def index #purpose of index is to put the array 
+    @products = Product.all.order(:name)
+    render "index.html.erb"
   end 
 
+  def new 
+    render "new.html.erb"
+  end 
+
+  def create
+    product = Product.new(
+    name: params[:form_name],
+    image: params[:form_image],
+    price: params[:form_price],
+    description: params[:form_description]
+    ) 
+    product.save
+    render "create.html.erb"
+  end 
+
+  def show #purpose of show is to show one product 
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "show.html.erb"
+  end 
+
+  def edit 
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render "edit.html.erb"
+  end 
+
+  def update 
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.name = params[:form_name]
+    @product.image = params[:form_image]
+    @product.price = params[:form_price]
+    @product.description = params[:form_description]
+    @product.save
+    render "update.html.erb"
+  end 
+
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.destroy
+    render "destroy.html.erb"
+  end 
 
 end
