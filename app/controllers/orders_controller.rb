dac2f4376e8307ee26884fbba7b40fb9f749e752
Rebecca Mutek <mutek.b@gmail.com>
2017-03-26 10:43:47 -0500

    complete order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,21 +4,22 @@ class OrdersController < ApplicationController
     @product = Product.find_by(id: params[:product_id])
     quantity = params[:quantity].to_i
     tax = (@product.price * 0.09).round(3) * quantity
+    
     order = Order.new(
     quantity: params[:quantity],
-    user_id: current_user.id,
     product_id: params[:product_id],
+    user_id: current_user.id,
     subtotal: @product.price * quantity,
     tax: @product.price * quantity * 0.09.round(3),
     total: (@product.price * quantity.to_i) + tax 
     ) 
     order.save
     flash[:success] = "Order Created!"
-    redirect_to "/orders/:id"
+    redirect_to "/orders/#{order.id}"
   end 
 
   def show
-
+    @order = Order.find_by(id: params[:id])
     render "show.html.erb"
   end 
 
